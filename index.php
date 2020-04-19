9172d4acc9e02e04683a01c552eb2541dfa336d8
sakinfens <sakinfens@gmail.com>
2020-04-18 21:46:41 -0400

    files

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
 echo 'Hello World';
+$host = "ec2-54-157-78-113.compute-1.amazonaws.com";
+$db = "dfeic2j4go38uv";
+$user = "urzbaunjgpyaic";
+$port = "5432";
+$password = "1aa711d99f72043f47fddb7f19841c0f917a4995a5fec7ccfc564ce22b1a65b4";
 
+
+
+
+$love = "host=". $host ."port=" . $port ."dbname=". $db;
+
+echo $love;
 
 ?>
